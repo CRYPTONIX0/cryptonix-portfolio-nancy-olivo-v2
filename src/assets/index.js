99f1e59e08f73c81eb791e00c2a3f9252163208a
Cryptonix Dev <cryptonix@cryptonix.dev>
2025-05-21 00:06:54 -0600

    Add files via upload

---
+++ src/assets/index.js
@@ -0,0 +1,114 @@
+import nancy from "./nancy-olivo.svg";
+import profilePic from "./perfil.jpg";
+import menu from "./menu.svg";
+import close from "./close.svg";
+import arrowUp from "./arrow-up.svg";
+import CCHA from "./CCHA.png";
+import cluboard from "./cluboard.png";
+import cash_flow from "./cash_flow.jpg";
+import imss from "./imss.png";
+import unamci from "./unamci.png";
+import iris from "./iris.png";
+import fba from "./fba.png";
+import portfolio from "./portfolio.jpg";
+import cdc from "./cdc.png";
+import chargeswap from "./chargeswap.png";
+import placeicon from "./place-icon.svg";
+import recruitment from "./recruitment.png";
+import huntly from "./huntly.png";
+import oracle from "./oracle.jpg";
+import comicify_ai from "./comicify-ai.png";
+import greentrust from "./green_trust.png";
+import averlon from "./averlon.jpg";
+import devfolio from "./devfolio.png";
+import cg from "./cg.png";
+import ethglobal from "./ethglobal.png";
+import polkadot from "./polkadot.jpeg";
+import lightspeed from "./lightspeed.png";
+import dennisivy from "./dennisivy.png";
+import manipal from "./manipal.png";
+import icon from "./icon.jpg";
+import ethforall from "./ethforall.png";
+import ethindia from "./ethindia.png";
+import globalAi from "./global-ai.jpeg";
+import facebook from './facebook.svg';
+import twitter from './twitter.svg';
+import instagram from './instagram.svg';
+import linkedin from './linkedin.svg';
+import tiktok from './tiktok.png';
+import reddit from './reddit.svg';
+import youtube from './youtube.svg';
+import fmsm from './fmsm.png';
+import mexicoSano from './mexicoSano.png';
+import psicologiaMexico from './psicologiaMexico.png';
+import inprfm from './inprfm.png';
+import saludMentalMexico from './saludMentalMexico.png';
+import elUniversal from './elUniversal.png';
+import milenio from './milenio.png';
+import excelsior from './excelsior.png';
+import forbesMexico from './forbesMexico.png';
+import noticierosTelevisa from './noticierosTelevisa.png';
+import animalPolitico from './animalPolitico.png';
+import elFinanciero from './elFinanciero.png';
+import medium from './medium.svg';
+import wordpress from './wordpress.svg';
+import psicoforum from './psicoforum.svg';
+
+// Exports grouped in a single block
+export {
+  menu,
+  close,
+  arrowUp,
+  CCHA,
+  cluboard,
+  cash_flow,
+  unamci,
+  iris,
+  imss,
+  fba,
+  portfolio,
+  cdc,
+  nancy,
+  profilePic,
+  chargeswap,
+  placeicon,
+  recruitment,
+  huntly,
+  oracle,
+  comicify_ai,
+  greentrust,
+  averlon,
+  devfolio,
+  cg,
+  ethglobal,
+  polkadot,
+  lightspeed,
+  dennisivy,
+  manipal,
+  icon,
+  ethforall,
+  ethindia,
+  globalAi,
+  facebook,
+  twitter,
+  instagram,
+  linkedin,
+  tiktok,
+  reddit,
+  youtube,
+  fmsm,
+  mexicoSano,
+  psicologiaMexico,
+  inprfm,
+  saludMentalMexico,
+  elUniversal,
+  milenio,
+  excelsior,
+  forbesMexico,
+  noticierosTelevisa,
+  animalPolitico,
+  elFinanciero,
+  medium,
+  wordpress,
+  psicoforum
+};
